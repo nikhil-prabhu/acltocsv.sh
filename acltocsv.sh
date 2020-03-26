322460fa1e459c17e85e7542597672c8494d88aa
Nikhil Prabhu <i516395@inll34397177b.apj.global.corp.sap>
2020-03-26 14:24:06 +0530

    Add function to extract data from webdisptab
        This function [_extract_webdisptab] extracts the
        required data block(s) from the web dispatcher ACL
        file and displays it.

--- acltocsv.sh
+++ acltocsv.sh
@@ -10,9 +10,44 @@ WEBDISPTAB="/usr/sap/WD2/whitelist/webdisptab_test"
 # SAP router ACL file
 SAPROUTTAB="/usr/sap/WD2/whitelist/saprouttab_test"
 
+# Temporary file to store extracted webdisptab entries
+TMP_WEBDISPTAB="/tmp/acltocsv.sh-webdisptab.temp"
+
+# Tell the script when to start reading data from the file
+READ_DATA="no"
+
 #############
 # FUNCTIONS #
 #############
+
+function _extract_webdisptab() { #quickdoc: Extracts the required data block(s) from the web dispatcher ACL file.
+
+    # LOCAL: Line read from ACL file
+    local _line
+    
+    while read _line
+    do
+	if [[ "$_line" =~ ^(##-- .*: .* --##)$ ]]
+	then
+	    READ_DATA="yes"
+	fi
+
+	if [[ "$_line" =~ ^(# Deny all other)$ ]]
+	then
+	    READ_DATA="no"
+	fi
+
+	if [ "$READ_DATA" = "yes" ]
+	then
+	    if [[ "$_line" =~ ^(##-- .*: .* --##)$ ]]
+	    then
+		echo "$_line" | sed -n -e "s/^.*.*: //p" | tr -d "\-\-##"
+	    else
+		echo "$_line"
+	    fi
+	fi
+    done < "$WEBDISPTAB"
+}
 
 function _convert_webdisptab() { #quickdoc: Converts the webdisptab file to a CSV file.
     echo &> /dev/null # Don't do anything for now
