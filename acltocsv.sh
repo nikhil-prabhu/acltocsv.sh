c4d157b5acb97854a064d9e6f443be7181de37cd
Nikhil Prabhu <i516395@inll34397177b.apj.global.corp.sap>
2020-03-26 13:07:49 +0530

    Add basic global variables and functions

---
+++ acltocsv.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+####################
+# GLOBAL VARIABLES #
+####################
+
+# Web dispatcher ACL file
+WEBDISPTAB="/usr/sap/WD2/whitelist/webdisptab_test"
+
+# SAP router ACL file
+SAPROUTTAB="/usr/sap/WD2/whitelist/saprouttab_test"
+
+#############
+# FUNCTIONS #
+#############
+
+function _convert_webdisptab() { #quickdoc: Converts the webdisptab file to a CSV file.
+    echo &> /dev/null # Don't do anything for now
+}
+
+function _convert_saprouttab() { #quickdoc: Converts the saprouttab file to a CSV file.
+    echo &> /dev/null # Don't do anything for now
+}
