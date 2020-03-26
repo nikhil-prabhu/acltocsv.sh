982c8ec98219d5c87a58155aa07c7d9d63a96d87
Nikhil Prabhu <i516395@inll34397177b.apj.global.corp.sap>
2020-03-26 20:50:31 +0530

    Extract data from webdisptab and convert it to CSV

--- acltocsv.sh
+++ acltocsv.sh
@@ -10,8 +10,8 @@ WEBDISPTAB="/usr/sap/WD2/whitelist/webdisptab_test"
 # SAP router ACL file
 SAPROUTTAB="/usr/sap/WD2/whitelist/saprouttab_test"
 
-# Temporary file to store extracted webdisptab entries
-TMP_WEBDISPTAB="/tmp/acltocsv.sh-webdisptab.temp"
+# CSV file for web dispatcher entries
+CSV_WEBDISPTAB="./webdisptab.csv"
 
 # Tell the script when to start reading data from the file
 READ_DATA="no"
@@ -24,7 +24,25 @@ function _extract_webdisptab() { #quickdoc: Extracts the required data block(s) 
 
     # LOCAL: Line read from ACL file
     local _line
+
+    # LOCAL: Name of partner for current block of entries
+    local _partner_name
+
+    # LOCAL: IP address
+    local _ip_address
+
+    # LOCAL: Employee id
+    local _employee_id
+
+    # LOCAL: Date of entry
+    local _entry_date
+
+    # LOCAL: Name of partner's technical consultant
+    local _consultant_name
     
+    # LOCAL: Technical consultant email id
+    local _consultant_email
+
     while read _line
     do
 	if [[ "$_line" =~ ^(##-- .*: .* --##)$ ]]
@@ -41,12 +59,22 @@ function _extract_webdisptab() { #quickdoc: Extracts the required data block(s) 
 	then
 	    if [[ "$_line" =~ ^(##-- .*: .* --##)$ ]]
 	    then
-		echo "$_line" | sed -e 's/[0-9]*[:]//g'
+		_partner_name=$(echo "$_line" | sed -e 's/[0-9]*[:]//g' | tr -d '#\-')
 	    else
-		echo "$_line"
+		_ip_address=$(echo "$_line" | awk '{print $5}')
+		_employee_id=$(echo "$_line" | grep -E "# .*: .*" | sed -n -e 's/^.*: //p' | awk -F "|" '{print $1}')
+		_entry_date=$(echo "$_line" | grep -E "# .*: .*" | sed -n -e 's/^.*: //p' | awk -F "|" '{print $2}')
+		_consultant_name=$(echo "$_line" | grep -E "# .*: .*" | sed -n -e 's/^.*: //p' | awk -F "|" '{print $3}')
+		_consultant_email=$(echo "$_line" | grep -E "# .*: .*" | sed -n -e 's/^.*: //p' | awk -F "|" '{print $4}')
+		echo "$_partner_name, $_ip_address , $_employee_id , $_entry_date , $_consultant_name , $_consultant_email" >> "$CSV_WEBDISPTAB"
 	    fi
 	fi
     done < "$WEBDISPTAB"
+
+    sort -o "$CSV_WEBDISPTAB" "$CSV_WEBDISPTAB"
+
+    sed -i '1s/^/\n/' "$CSV_WEBDISPTAB"
+    sed -i '1s/^/PARTNER NAME , IP ADDRESS , EMPLOYEE ID , ENTRY DATE , CONSULTANT , EMAIL ID\n/' "$CSV_WEBDISPTAB"
 }
 
 function _convert_webdisptab() { #quickdoc: Converts the webdisptab file to a CSV file.
